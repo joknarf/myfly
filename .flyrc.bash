32a10875a1698a2cbea0050f353536e2375ac697
joknarf <joknarf@free.fr>
2024-09-22 12:56:26 +0200

    fix nerdp

---
+++ .flyrc.bash
@@ -0,0 +1,7 @@
+echo "joknarf .flyrc.bash"
+HISTCONTROL=ignorespace
+HISTIGNORE="cd:cd *:history:ls:l:pwd:df:df -k:kill *:top:clear"
+shopt -s histappend
+PROMPT_COMMAND='history -a'
+[ -f /etc/bash_completion ] && ! complete -p git >/dev/null 2>&1 && . /etc/bash_completion
+bind -m vi-insert '"²": "\e"'
---
+++ .flyrc.zsh
@@ -0,0 +1,17 @@
+echo "joknarf .flyrc.zsh" >&2
+skip_global_compinit=1
+autoload -Uz compinit
+compinit
+
+# Lines configured by zsh-newuser-install
+HISTFILE=~/.zsh_history
+HISTSIZE=2000
+SAVEHIST=1000
+setopt appendhistory
+bindkey -e
+# End of lines configured by zsh-newuser-install
+# The following lines were added by compinstall
+#zstyle :compinstall filename '/home/joknarf/.zshrc'
+
+#setopt share_history
+setopt incappendhistory
