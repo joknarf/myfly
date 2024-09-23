ceeeb9860d61ff0144e8d03191bdcfa4dcd486d7
joknarf <joknarf@free.fr>
2024-09-23 20:53:43 +0200

    mac bash zsh

--- .flyrc.bash
+++ .flyrc.bash
@@ -4,4 +4,5 @@ HISTIGNORE="cd:cd *:history:ls:l:pwd:df:df -k:kill *:top:clear"
 shopt -s histappend
 PROMPT_COMMAND='history -a'
 [ -f /etc/bash_completion ] && ! complete -p git >/dev/null 2>&1 && . /etc/bash_completion
+[ -f /opt/homebrew/etc/bash_completion ] && . /opt/homebrew/etc/bash_completion
 bind -m vi-insert '"²": "\e"'
--- .flyrc.zsh
+++ .flyrc.zsh
@@ -5,13 +5,23 @@ compinit
 
 # Lines configured by zsh-newuser-install
 HISTFILE=~/.zsh_history
-HISTSIZE=2000
-SAVEHIST=1000
+HISTSIZE=20000
+SAVEHIST=10000
 setopt appendhistory
-bindkey -e
+# bindkey -e
 # End of lines configured by zsh-newuser-install
 # The following lines were added by compinstall
 #zstyle :compinstall filename '/home/joknarf/.zshrc'
 
 #setopt share_history
 setopt incappendhistory
+[ -f /opt/homebrew/etc/bash_completion ] && . /opt/homebrew/etc/bash_completion
+[ -d /opt/homebrew ] && {
+    export HOMEBREW_PREFIX="/opt/homebrew";
+    export HOMEBREW_CELLAR="/opt/homebrew/Cellar";
+    export HOMEBREW_REPOSITORY="/opt/homebrew";
+    export PATH="/opt/homebrew/bin:/opt/homebrew/sbin${PATH+:$PATH}";
+    export MANPATH="/opt/homebrew/share/man${MANPATH+:$MANPATH}:";
+    export INFOPATH="/opt/homebrew/share/info:${INFOPATH:-}";
+    FPATH=$(brew --prefix)/share/zsh-completions:$FPATH
+}
