2b0752f5ff4fab685346e9a19be83001833edf34
joknarf <joknarf@free.fr>
2024-09-23 21:51:19 +0200

    ish

--- .flyrc.bash
+++ .flyrc.bash
@@ -6,3 +6,4 @@ PROMPT_COMMAND='history -a'
 [ -f /etc/bash_completion ] && ! complete -p git >/dev/null 2>&1 && . /etc/bash_completion
 [ -f /opt/homebrew/etc/bash_completion ] && . /opt/homebrew/etc/bash_completion
 bind -m vi-insert '"²": "\e"'
+bind -m vi-insert '"ù": "\eA'"'"'\eI.\eI\C-xs'"'"'\C-xm\e0xi\C-xc\ei.\C-xk$(_cdpr=1 cdhist + '"'"'\C-y\C-h'"'"')\C-xe\eI\C-xx\eA\C-xr\015"'
