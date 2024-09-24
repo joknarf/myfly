e81a892f3103550af6f83b3cbdfc2aeb13647a15
joknarf <joknarf@free.fr>
2024-09-24 18:55:04 +0200

    newfly

--- .flyrc.bash
+++ .flyrc.bash
@@ -6,3 +6,4 @@ PROMPT_COMMAND='history -a'
 [ -f /etc/bash_completion ] && ! complete -p git >/dev/null 2>&1 && . /etc/bash_completion
 [ -f /opt/homebrew/etc/bash_completion ] && . /opt/homebrew/etc/bash_completion
 #bind -m vi-insert '"²": "\e"'
+bind 'set enable-bracketed-paste off'
